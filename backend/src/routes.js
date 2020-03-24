1d306facdc251109f6ff8aec68976a0d474ba638
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-03-24 22:04:40 +0000

    delete incident

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,11 +5,10 @@ const IncidentController = require("./controllers/IncidentController");
 const routes = express.Router();
 
 routes.get("/ongs", OngController.index);
-
 routes.post("/ongs", OngController.create);
 
 routes.get("/incidents", IncidentController.index);
-
 routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = routes;
