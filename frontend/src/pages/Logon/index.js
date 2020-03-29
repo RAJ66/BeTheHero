2b5c6a8df931accadaa10b3edadb112464710373
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-03-29 20:49:47 +0100

    connect login to api

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
+
+import api from "../../services/api";
+
 import "./styles.css";
 
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
 
 export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("sessions", { id });
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+
+      console.log(response.data.name);
+
+      history.push("/profile");
+    } catch (error) {
+      alert("Error in login, please try again.");
+      console.log(error);
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Login</h1>
-          <input type="Your ID" />
+          <input
+            type="Your ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button type="submit" className="button">
             Enter
           </button>
