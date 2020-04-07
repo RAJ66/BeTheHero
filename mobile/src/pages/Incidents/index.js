6596d9cc95dc669a5468017f9cbb691bdbed84cb
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-04-07 18:04:46 +0100

    static incident page complete

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
-
+import { FlatList, View, Image, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import styles from "./styles";
 import logoImg from "../../assets/logo.png";
 
+import { useNavigation } from "@react-navigation/native";
+
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate("Detail");
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -17,6 +25,32 @@ export default function Incidents() {
       <Text style={styles.descrition}>
         Choose one of the incidents and save the day.
       </Text>
+      <FlatList
+        style={styles.incidentList}
+        data={[1, 2, 3]}
+        keyExtractor={(indident) => String(indident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>Apad</Text>
+
+            <Text style={styles.incidentProperty}>INCIDENT:</Text>
+            <Text style={styles.incidentValue}>cat</Text>
+
+            <Text style={styles.incidentProperty}>Value:</Text>
+            <Text style={styles.incidentValue}>120€</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}
+            >
+              <Text style={styles.detailsButtonText}>See more details</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
